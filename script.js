a1604cc3fd28ec946af16059801cb5cc8e5ec52b
Taranjit Kaur <taranjit.kaur@venturepact.com>
2025-02-14 09:06:00 +0530

    Upto Testimonials

---
+++ script.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper('.tst-swiper', {
+    loop: true,
+    navigation: {
+        nextEl: '.ri-arrow-right-line',  // Corrected "nextE1" → "nextEl"
+        prevEl: '.ri-arrow-left-line'   // Corrected "prevE1" → "prevEl"
+    }
+});
