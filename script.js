512705dffc7e302f68514f227b705a8b229c34ec
Taranjit Kaur <taranjit.kaur@venturepact.com>
2025-02-14 09:32:53 +0530

    testimonials

--- script.js
+++
@@ -1,7 +0,0 @@
-var swiper = new Swiper('.tst-swiper', {
-    loop: true,
-    navigation: {
-        nextEl: '.ri-arrow-right-line',  // Corrected "nextE1" → "nextEl"
-        prevEl: '.ri-arrow-left-line'   // Corrected "prevE1" → "prevEl"
-    }
-});
